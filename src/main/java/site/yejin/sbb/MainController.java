65111957380fef84af6dd59b27f4ea0999e349c8
kyj2212 <kyj2212@gmail.com>
2022-08-08 11:32:19 +0900

    CHORE: git init

---
+++ src/main/java/site/yejin/sbb/MainController.java
@@ -0,0 +1,15 @@
+package site.yejin.sbb;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/sbb")
+    public void index(){
+        System.out.println("index");
+    }
+
+}
