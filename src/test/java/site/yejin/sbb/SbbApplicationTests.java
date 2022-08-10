3da9d773df3f33af8a6cbccce4e0e702b22c8683
kyj2212 <kyj2212@gmail.com>
2022-08-10 15:38:47 +0900

    entity crud with tdd and jpa

--- src/test/java/site/yejin/sbb/SbbApplicationTests.java
+++ src/test/java/site/yejin/sbb/SbbApplicationTests.java
@@ -5,6 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -28,6 +34,59 @@ class SbbApplicationTests {
         q2.setContent("id는 자동으로 생성되나요?");
         q2.setCreateDate(LocalDateTime.now());
         questionRepository.save(q2);  // 두번째 질문 저장
+
+        assertThat(q1.getId()).isGreaterThan(0);
+        assertThat(q2.getId()).isGreaterThan(q1.getId());
+    }
+
+    @Test
+    void testJpa2(){
+        List<Question> all = questionRepository.findAll();
+        System.out.println(all);
+        assertEquals(2, all.size());
+
+        Question q = all.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
+
+    @Test
+    void testJpa3(){
+        Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(1,q.getId());
+    }
+    @Test
+    void testJpa4() {
+        Question q = this.questionRepository.findBySubjectAndContent(
+                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+        assertEquals(1, q.getId());
+    }
+    @Test
+    void testJpa5() {
+        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+        Question q = qList.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
+
+    @Test
+    void testJpa6(){
+        Optional<Question> oq = this.questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        q.setSubject("수정된 제목");
+        System.out.println(q.getSubject());
+        this.questionRepository.save(q);
+
+    }
+
+    @Test
+    void testJpa7(){
+        long cntOrg = this.questionRepository.count();
+        Optional<Question> oq = this.questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        this.questionRepository.delete(q);
+        System.out.println(this.questionRepository.existsById(1));
+        assertEquals(cntOrg-1,this.questionRepository.count() );
     }
 
 }
