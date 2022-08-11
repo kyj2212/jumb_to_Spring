c6f8c0b5882fbdbf93ee7538fb565fcffb8a70f1
kyj2212 <kyj2212@gmail.com>
2022-08-12 00:11:25 +0900

    Style: line align, and comment

--- src/test/java/site/yejin/sbb/QnARepositoryTests.java
+++ src/test/java/site/yejin/sbb/QnARepositoryTests.java
@@ -1,19 +1,17 @@
 package site.yejin.sbb;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
-import site.yejin.sbb.base.RepositoryUtil;
 
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,35 +25,61 @@ import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
 @SpringBootTest
 @TestInstance(PER_CLASS)
-class SbbApplicationTests {
+class QnARepositoryTests {
+
     @Autowired
     private QuestionRepository questionRepository; // questionRepo 는 sbbtest 클래스가 생성될때 같이 생성되어야 하는 필드이다. // qeustionController, service를 만든다면 이 필드가 들어가겟지
     @Autowired
     private AnswerRepository answerRepository;
-
-
     @PersistenceContext
     EntityManager entityManager;
 
-    private static int lastTestDataId;
+
+
+    private static int lastTestQuestionId;
+    private static int lastTestAnswerId;
     private List<String> tableNames;
+
+
     @Test
     void contextLoads() {
     }
 
 
-
-    @BeforeAll
-    public void beforeAll() {
+    //
+    @Test
+    @Transactional
+    public void testEMExecute() {
 
         tableNames=new ArrayList<>();
         tableNames.add("question");
         tableNames.add("answer");
 
+        entityManager.flush();
+        entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS=0").executeUpdate();
+
+
+        Query q = entityManager.createNativeQuery("SELECT @@session.foreign_key_checks");
+       // Query q = entityManager.createNativeQuery("SELECT @@global.foreign_key_checks,@@session.foreign_key_checks");
+        List list = q.getResultList();
+        System.out.println(list.get(0));
+
+
+        assertEquals(BigInteger.ZERO, list.get(0));
+            //System.out.println(i);
+            // 테이블의 모든 row 삭제
+            // entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
+
+        entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS=1").executeUpdate();
+
     }
 
 
-/*
+
+
+    // truncate 할때 EntityManager.createNativeQuery() 를 쓰고 싶었으나, transitional 이 보장되지 않아 에러 발생
+    // 현재 구조상, Qeustion과 Answer repo의 공통을 묶는 RepoUtil 인터페이스에서 default 메서드로 구현하는 것이 가장 적합
+    /*
     @BeforeEach
     public void beforeEachTruncate() {
         System.out.println("이거 안돼?");
@@ -75,58 +99,38 @@ class SbbApplicationTests {
     }
 */
 
+
+    /* --------- BeforeEach 시작 --------------*/
+    // 테스트 케이스 시작하기 전에 테이블을 truncate 하고 테스트용 데이터 2개 튜플씩 insert
     @BeforeEach
     public void beforeEach() {
         truncateTable();
         createTestData();
     }
 
-
-    @Transactional
-    public void execute() {
-        entityManager.flush();
-        entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS=0").executeUpdate();
-
-        for (String tableName : tableNames) {
-            // 테이블의 모든 row 삭제
-            entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
-        }
-        entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS=1").executeUpdate();
-    }
-
-
+    // truncate
     private void truncateTable() {
-
         truncateQuestionTable();
         truncateAnswerTable();
-
-
     }
-
-
     public void truncateQuestionTable() {
         this.questionRepository.truncate();
-        //this.questionRepository.setForeignKeyChecks(1);
     }
     public void truncateAnswerTable() {
-        // answers 는 foreign key가 없다!
         this.answerRepository.truncate();
     }
 
-    // create Test Data
 
+    // create Test Data
     public void createTestData() {
         insertQuestionTestData();
         insertAnswerTestData();
     }
-
-
     private void insertQuestionTestData() {
         Question q1 = new Question();
         q1.setSubject("sbb가 무엇인가요?");
         q1.setContent("sbb에 대해서 알고 싶습니다.");
         q1.setCreateDate(LocalDateTime.now());
-        // this를 하는이유?
         this.questionRepository.save(q1);  // 첫번째 질문 저장
 
         Question q2 = new Question();
@@ -134,12 +138,8 @@ class SbbApplicationTests {
         q2.setContent("id는 자동으로 생성되나요?");
         q2.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q2);  // 두번째 질문 저장
-
-        lastTestDataId=q2.getId();
+        lastTestQuestionId =q2.getId();
     }
-
-
-
     private void insertAnswerTestData() {
         Question q1 = findQuestionById(1);
         Answer a1 = new Answer();
@@ -154,8 +154,8 @@ class SbbApplicationTests {
         a2.setQuestion(q2);
         a2.setCreateDate(LocalDateTime.now());
         this.answerRepository.save(a2);
+        lastTestAnswerId=a2.getId();
     }
-
     private Question findQuestionById(int id) {
         Optional<Question> oq = this.questionRepository.findById(id);
         if(oq.isPresent()){
@@ -163,11 +163,13 @@ class SbbApplicationTests {
             return q;
         } else
             return null;
-//        List<Question> all = this.questionRepository.findAll();
- //       System.out.println(all);
     }
 
+    /* --------- BeforeEach 끝 --------------*/
 
+
+
+    // -------------Truncate 테이블 테스트 시작-----------
     @Test
     public void testJpaTruncateTableQ(){
         this.questionRepository.setForeignKeyChecks(0);
@@ -179,6 +181,7 @@ class SbbApplicationTests {
         this.answerRepository.truncate();
     }
 
+    // 단순히 answer 테이블의 외래키 제약을 지워버림으로써 truncate 처리
 /*
     @Test
     public void testJpaTruncateTableAnswerNoConstraint(){
@@ -186,7 +189,10 @@ class SbbApplicationTests {
         this.questionRepository.truncateTable();
     }
 */
+    // -------------Truncate 테이블 테스트 끝-----------
 
+
+    // -------------Insert 테스트 시작-----------
     @Test
     void testJpaInsertQuestion() {
         Question q = new Question();
@@ -196,14 +202,9 @@ class SbbApplicationTests {
         this.questionRepository.save(q);
         assertThat(q.getId()).isGreaterThan(0);
     }
-
-
-
     @Test
     public void testJpaInsertIntoAnswer() {
-        insertAnswerTestData();
-
-        Optional<Question> oq = this.questionRepository.findById(lastTestDataId);
+        Optional<Question> oq = this.questionRepository.findById(lastTestQuestionId);
         assertTrue(oq.isPresent());
         Question q= oq.get();
        // System.out.println(q.getContent());
@@ -216,25 +217,20 @@ class SbbApplicationTests {
       //  int size =q.getAnswerList().size();
       //  System.out.println("답변 추가후 질문에 있는 답변 개수 : "+size+" 넣은 답변 : "+q.getAnswerList().get(size-1).getContent());
     }
+    // -------------Insert 테스트 끝-----------
 
+
+    // -------------Select 테스트 시작-----------
+
+    // ------Question select 시작-------
     @Test
-    void testJapSelectAll(){
-        // this를 하는이유?
+    void testJapSelectAllQuestion(){
         List<Question> all = this.questionRepository.findAll();
         System.out.println(all.size());
         for(Question q : all){
             System.out.println(q.getSubject());
         }
     }
-
-    @Test
-    void testJpaFindbyId(){
-        Optional<Question> oq = this.questionRepository.findById(1);
-        assertTrue(oq.isPresent());
-        Question q= oq.get();
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
-    }
-
     @Test
     void testJpaFindAllGetId(){
         // this를 하는이유?
@@ -245,7 +241,13 @@ class SbbApplicationTests {
         Question q = all.get(0);
         assertEquals("sbb가 무엇인가요?", q.getSubject());
     }
-
+    @Test
+    void testJpaFindById(){
+        Optional<Question> oq = this.questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q= oq.get();
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
     @Test
     void testJpaFindBySubject(){
         // this를 하는 이유
@@ -264,7 +266,28 @@ class SbbApplicationTests {
         Question q = qList.get(0);
         assertEquals("sbb가 무엇인가요?", q.getSubject());
     }
+    // ------Question select 끝-------
 
+    // ------Answer select 시작-------
+    @Test
+    public void testJpaSelectAllAnswer(){
+        List<Answer> all = this.answerRepository.findAll();
+        System.out.println(all);
+        assertEquals(2, all.size());
+    }
+    @Test
+    public void testJpaSelectFindAnswerById(){
+        Optional<Answer> oa = this.answerRepository.findById(lastTestAnswerId);
+        assertTrue(oa.isPresent());
+        Answer a = oa.get();
+        assertEquals("id는 자동으로 생성됩니다.",a.getContent());
+    }
+
+
+    // ------Answer select 끝-------
+    // -------------Select 테스트 끝-----------
+
+    // -------------Update 테스트 시작-----------
     @Test
     void testJpaUpdate(){
         Optional<Question> oq = this.questionRepository.findById(1);
@@ -275,7 +298,10 @@ class SbbApplicationTests {
         this.questionRepository.save(q);
 
     }
+    // -------------Update 테스트 끝-----------
 
+
+    // -------------Delete 테스트 시작-----------
     @Test
     void testJpaDeleteQuestion(){
         long cntOrg = this.questionRepository.count();
@@ -307,12 +333,13 @@ class SbbApplicationTests {
         System.out.println(q.getId()+" " +q.getSubject() + " exist? : "+ this.questionRepository.existsById(q.getId()));
         assertEquals(cntOrg-1,this.questionRepository.count() );
     }
+    // -------------Delete 테스트 끝-----------
 
 
+
+    // @Query( nativeQuery) 테스트
     @Test
     public void testJpaQueryParam(){
-
-
         //this.questionRepository.selectFromQMark("question");
         //this.questionRepository.selectFromParamName("question");
         //this.questionRepository.selectFromParamAnno("question");
@@ -323,16 +350,4 @@ class SbbApplicationTests {
 
 
 
-/*
-    @Test
-    public void testJpaSelectAnswer(){
-
-        List<Answer> all = this.answerRepository.findAll();
-        System.out.println(all);
-        assertEquals(2, all.size());
-
-
-        //assertEquals("sbb가 무엇인가요?", q.getSubject());
-
-    }*/
 }
